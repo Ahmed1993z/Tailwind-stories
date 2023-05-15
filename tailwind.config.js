f425b29ea675b9d8062902111f68bd3a98fe498c
unknown <zoghlamiahmed1993@gmail.com>
2023-05-16 00:04:13 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        fuchsia: colors.fuchsia,
+      },
+      // ...
+    },
+  },
+  plugins: [],
+}
